be63822d74006075235c437ef1f3fcaf16840929
acparkgh <acpark123@gmail.com>
2021-03-03 05:19:40 -0500

    updated Reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,6 +4,23 @@ const initialState = {
 }
 
 const reducer = (state = initialState, action) => {
+  if(action.type === 'INCREMENT') {
+    return {
+      ...state, counter: state.counter + 1
+    }
+  } else if(action.type === 'DECREMENT') {
+    return {
+      ...state, counter: state.counter - 1
+    }
+  } else if(action.type === 'ADDFIVE') {
+    return {
+      ...state, counter: state.counter + 5
+    }
+  } else if(action.type === 'SUBTRACTFIVE') {
+    return {
+      ...state, counter: state.counter - 5
+    }
+  }
   return state;
 }
 
