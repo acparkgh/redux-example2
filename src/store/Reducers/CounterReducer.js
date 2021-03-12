fbabe379745c186da660eb8a2a7b3ba4014214c5
acparkgh <acpark123@gmail.com>
2021-03-12 17:45:10 -0500

    using utility function in reducers

--- src/store/Reducers/CounterReducer.js
+++ src/store/Reducers/CounterReducer.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from './utility';
 
 const initialState = {
   counter: 0,
@@ -19,11 +20,14 @@ const reducer = ( state = initialState, action ) => {
       return {
         ...state, counter: state.counter + action.val        
       }
+    // case actionTypes.SUBTRACT:
+    //   return {
+    //     ...state, counter: state.counter - action.val        
+    //   }
     case actionTypes.SUBTRACT:
-      return {
-        ...state, counter: state.counter - action.val        
-      }
-  }
+      return updateObject( state, { counter: state.counter - action.val } )
+
+  }  
 
   return state;
 }
